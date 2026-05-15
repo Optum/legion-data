50d2b48f729ade69de433640bb0eb7dceac92233
Esity <matthewdiverson@gmail.com>
2026-05-15 17:50:39 -0500

    fix(migrations): 100 creates bare tables on non-postgres; 101-102 run on all adapters without guards

--- lib/legion/data/migrations/102_add_apollo_identity_indexes.rb
+++ lib/legion/data/migrations/102_add_apollo_identity_indexes.rb
@@ -2,16 +2,12 @@
 
 Sequel.migration do
   up do
-    next unless adapter_scheme == :postgres
-
     run 'CREATE INDEX idx_apollo_access_scope ON apollo_entries (access_scope)'
     run 'CREATE INDEX idx_apollo_identity_principal_id ON apollo_entries (identity_principal_id) WHERE identity_principal_id IS NOT NULL'
     run 'CREATE INDEX idx_apollo_identity_id ON apollo_entries (identity_id) WHERE identity_id IS NOT NULL'
   end
 
   down do
-    next unless adapter_scheme == :postgres
-
     run 'DROP INDEX IF EXISTS idx_apollo_access_scope'
     run 'DROP INDEX IF EXISTS idx_apollo_identity_principal_id'
     run 'DROP INDEX IF EXISTS idx_apollo_identity_id'
