7b7c2d1c1143b7f3046c79348294bc18ed4d2357
Esity <matthewdiverson@gmail.com>
2026-06-01 15:12:51 -0500

    fix(migration): guard drop_index in migration 116 down with if_exists

--- lib/legion/data/migrations/116_make_tool_calls_response_id_nullable.rb
+++ lib/legion/data/migrations/116_make_tool_calls_response_id_nullable.rb
@@ -16,7 +16,7 @@ Sequel.migration do
 
   down do
     alter_table(:llm_tool_calls) do
-      drop_index :identity_principal_id, name: :idx_tool_calls_identity_principal_id
+      drop_index :identity_principal_id, name: :idx_tool_calls_identity_principal_id, if_exists: true
     end
     alter_table(:llm_tool_calls) do
       set_column_not_null :message_inference_response_id
